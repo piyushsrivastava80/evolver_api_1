4534cec29368750571c43a2df815a0a708395548
piyushsrivastava80 <psrivastava@bioxcel.com>
2023-05-19 17:04:28 +0530

    add

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const pg = require('pg');
 const connection = new pg.Pool({// apllication database connection
     // const connection = new pg.Client({// apllication database connection
     user: 'tracker',
-    host: 'bxl-db.c6ypiqv0awyo.us-east-1.rds.amazonaws.coms',
+    host: 'bxl-db.c6ypiqv0awyo.us-east-1.rds.amazonaws.com',
     database: 'innotracker',
     password: 'fghjk%$^&hgjk',
     port: 5432,
